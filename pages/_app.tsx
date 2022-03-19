1f1b9fe82efb5067f970e2af0a91de5bd3805c12
Kazuaki <kazuaki.hirata.0905@gmail.com>
2022-03-19 16:23:37 +0900

    Install Font Awesome

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,9 @@ import "sanitize.css/forms.css";
 import "sanitize.css/typography.css";
 import "../styles/globals.css";
 import "../styles/utils.module.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+config.autoAddCss = false;
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
