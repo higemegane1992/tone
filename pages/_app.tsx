c4d36b6ed79f3f2afb8b9ec13f1b64c34fc4489b
Kazuaki <kazuaki.hirata.0905@gmail.com>
2022-03-19 16:22:22 +0900

    Add a utility module css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import "sanitize.css";
 import "sanitize.css/forms.css";
 import "sanitize.css/typography.css";
 import "../styles/globals.css";
+import "../styles/utils.module.css";
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
