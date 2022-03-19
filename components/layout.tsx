1dbf86fdadcd1389dc221278183a7e224e7e0c9d
Kazuaki <kazuaki.hirata.0905@gmail.com>
2022-03-19 13:47:56 +0900

    Create a layout component

---
+++ components/layout.tsx
@@ -0,0 +1,28 @@
+import React, { ReactNode } from "react";
+import Head from "next/head";
+import styles from "./layout.module.css";
+
+type Props = {
+  title: string;
+  description: string;
+  children: ReactNode;
+};
+
+const Layout: React.FC<Props> = (props) => (
+  <>
+    <Head>
+      <title>
+        {props.title ? props.title : "tone ――つくる、つながる、とどける。"}
+      </title>
+      {props.description ? (
+        <meta name="description" content={props.description} />
+      ) : (
+        <></>
+      )}
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <main className={styles.container}>{props.children}</main>
+  </>
+);
+
+export default Layout;
