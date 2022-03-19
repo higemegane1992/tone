c3fc79f753c053ebd62809dfe8ab424b790fc400
Kazuaki <kazuaki.hirata.0905@gmail.com>
2022-03-20 00:18:56 +0900

    Remove utils.module.css from _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import "sanitize.css";
 import "sanitize.css/forms.css";
 import "sanitize.css/typography.css";
 import "../styles/globals.css";
-import "../styles/utils.module.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 config.autoAddCss = false;
